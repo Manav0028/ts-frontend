c9fd896b30149296dcc06e9aae5c2af43c277b97
Manav Sharma <ishmanav@gmail.com>
2023-09-20 00:53:18 +1000

    Chart with time series from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { createChart } from 'lightweight-charts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function CandlestickChart() {
+    const [candlestickData, setCandlestickData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [chartInstance, setChartInstance] = useState(null); // Track the chart instance
+
+    useEffect(() => {
+        // Fetch candlestick data from your API or source
+        axios.get('http://localhost:8080/api/charts/NASDAQ/AAPL')
+            .then((response) => {
+                setCandlestickData(response.data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (!isLoading && !chartInstance) {
+            // Create a new chart instance
+            const chart = createChart('chart-container', { width: 800, height: 400 });
+
+            // Create a new candlestick series
+            const candlestickSeries = chart.addCandlestickSeries();
+
+            // Add candlestick data to the series
+            candlestickSeries.setData(candlestickData);
+
+            // Save the chart instance
+            setChartInstance(chart);
+        }
+    }, [candlestickData, isLoading, chartInstance]);
+
+    return (
+        <div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <div id="chart-container" />
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default CandlestickChart;
